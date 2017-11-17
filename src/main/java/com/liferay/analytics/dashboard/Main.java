1d3f79119f205beda5b302a8cf95a98e7239ee17
Marcellus Tavares <marcellus.tavares@liferay.com>
2017-11-17 15:01:13 -0300

    analytics-dashboard

---
+++ src/main/java/com/liferay/analytics/dashboard/Main.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.dashboard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Marcellus Tavares
+ */
+@SpringBootApplication
+@Controller
+public class Main {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "welcome";
+
+	}
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
+    private static final Logger _log = LoggerFactory.getLogger(Main.class);
+
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,15 @@
+
+<!doctype html>
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Analytics Dashboard</title>
+  </head>
+  <body>
+  	Hello World <%= "Dash" %>!
+  </body>
+</html>
