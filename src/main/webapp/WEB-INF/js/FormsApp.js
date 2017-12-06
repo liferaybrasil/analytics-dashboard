4ac9427c5b9c4efe157cd968413080a30f89aec1
Leonardo Barros <leonardo.barros@liferay.com>
2017-12-06 17:40:42 -0300

    Update forms dashboard

--- src/main/webapp/WEB-INF/js/FormsApp.js
+++ src/main/webapp/WEB-INF/js/FormsApp.js
@@ -1,39 +1,108 @@
 import JSXComponent from 'metal-jsx';
 
+import Ajax from 'metal-ajax';
+
 import '../css/main.scss';
 
 class Dashboard extends JSXComponent {
-
+	attached() {
+		this.callService();
+		
+		setInterval(() => {
+			this.callService();
+		}, 60000);
+		
+	}
+	
+	callService() {
+		Ajax.request(location.origin + '/api/forms/find?analyticskey=analyticskey:test&formid=35490&start=2017-12-04').
+			then((xhr) => {
+				let json = JSON.parse(xhr.response);
+				let sessions = 0;
+				
+				json.forEach(function (value) {
+					sessions += value.sessions;
+				});
+				
+				this.props.sessions = sessions;
+			});
+	}
+	
 	render() {
 		return (
 			<div>
 				<Header text='Hello'></Header>
-				<Summary></Summary>
+				<div class="container-fluid">
+					<section class="row">
+						<Sessions sessions={this.props.sessions}></Sessions>
+						<div>Leo</div>
+						<div>Leo</div>
+					</section>
+				</div>
 			</div>
 		);
 	}
 }
 
+Dashboard.PROPS = {
+	sessions: {
+		value: 0
+	}
+}
+
 var Header = ({ text }) => {
 	return ( 
-		<header class="header">{text}</header>
+		<header class="header">
+			<h1 class="container-fluid">
+				{text}
+			</h1>
+		</header>
 	);
 };
 
-class Summary extends JSXComponent {
+class Sessions extends JSXComponent {
 
 	render() {
 		return (
-			<div class="box">
-				<h2>{this.state.sessions}</h2>
-				<h2>{this.state.started}</h2>
-				<h2>{this.state.converted}</h2>
-			</div>
+			<article class="col-md-6 box">
+				<div class="sessions-container">
+					<h2>
+						<span>
+							{this.props.sessions}
+						</span>
+					</h2>
+					<h3 class="text-center">
+						SESSIONS
+					</h3>
+				</div>
+
+				<div class="sessions-container">
+					<h2>
+						<span>
+							{this.props.started}
+						</span>
+					</h2>
+					<h3 class="text-center">
+						STARTED
+					</h3>
+				</div>
+
+				<div class="sessions-container">
+					<h2>
+						<span>
+							{this.props.converted}
+						</span>
+					</h2>
+					<h3 class="text-center">
+						CONVERTED
+					</h3>
+				</div>
+			</article>
 		); 
 	}
 }
 
-Summary.STATE = {
+Sessions.PROPS = {
 	sessions: {
 		value: 0
 	},
