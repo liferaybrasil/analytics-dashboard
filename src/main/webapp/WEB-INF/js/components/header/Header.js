e2d7b507ee8fb81c7eb4dc163ba192326c3e81cc
Adriano Interaminense <adriano.interaminense@liferay.com>
2017-12-07 23:35:19 -0300

    Add option to insert icon on header

--- src/main/webapp/WEB-INF/js/components/header/Header.js
+++ src/main/webapp/WEB-INF/js/components/header/Header.js
@@ -1,4 +1,6 @@
 import JSXComponent from 'metal-jsx';
+import {Icon} from './../../components';
+
 import './Header.scss';
 
 class Header extends JSXComponent {
@@ -6,7 +8,12 @@ class Header extends JSXComponent {
         return (
             <header class="header">
                 <div class="container-fluid">
-                    <h2 class="header__title">{this.props.title}</h2>
+                    <h2 class="header__title">
+                        <span class="header__icon">
+                            <Icon symbol={this.props.icon} />
+                        </span>
+                        {this.props.title}
+                    </h2>
                 </div>
             </header>
         );
