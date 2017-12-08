803c161bef46601f3fc08cafbf462dc993cad382
Adriano Interaminense <adriano.interaminense@liferay.com>
2017-12-07 23:42:12 -0300

    Update component Button adding parameter icon

--- src/main/webapp/WEB-INF/js/components/Button/Button.js
+++ src/main/webapp/WEB-INF/js/components/Button/Button.js
@@ -1,10 +1,23 @@
 import JSXComponent from 'metal-jsx';
+import {Icon} from './../../components';
+
 import './Button.scss';
 
 class Button extends JSXComponent {
+    renderIcon() {
+        if(!this.props.iconSymbol) {
+            return;
+        }
+        
+        return (
+            <Icon symbol={this.props.iconSymbol} />
+        );
+    }
+
     render() {
         return (
             <button type="button" class={`btn btn-dashboard btn-${this.props.style}`}>
+                {this.renderIcon()}
                 {this.props.label}
             </button>
         );
