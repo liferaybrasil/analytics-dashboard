34b3912e3251b8b26b80961a67c91fcf6c2d5db4
Adriano Interaminense <adriano.interaminense@liferay.com>
2017-12-09 01:52:18 -0300

    Add component ArrowIndicator

--- src/main/webapp/WEB-INF/js/components/index.js
+++ src/main/webapp/WEB-INF/js/components/index.js
@@ -2,7 +2,16 @@ import Header from './Header';
 import Card from './Card';
 import Icon from './Icon';
 import Button from './Button';
+import ArrowIndicator from './ArrowIndicator';
 import Dropdown from './Dropdown';
 import Footer from './Footer';
 
-export {Header, Card, Icon, Button, Dropdown, Footer};
+export {
+	Header,
+	Card,
+	Icon,
+	Button,
+	ArrowIndicator,
+	Dropdown,
+	Footer,
+};
